85ea61a8594e540e66eb0ec1148e224220ebc6f4
Prasanth <prashanth702@gmail.com>
2015-03-09 20:46:29 -1200

    Added bind Once and UnBind features

--- Codehard.CodeSharp/Program.cs
+++ Codehard.CodeSharp/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ActionType = System.Action<object>;
+using ActionType = System.Tuple<System.Action<object>, bool>;
+using CollectionType = System.Tuple<System.Guid, string, System.Collections.Generic.List<System.Tuple<System.Action<object>, bool>>>;
 
 namespace Codehard.CodeSharp
 {
     public class Emitter
     {
-        private static List<Tuple<Guid, string, List<ActionType>>> EventCollection { get; set; }
+        private static List<CollectionType> EventCollection { get; set; }
         private Guid ID { get; set; }
 
         static Emitter()
         {
-            EventCollection = new List<Tuple<Guid, string, List<ActionType>>>();
+            EventCollection = new List<CollectionType>();
         }
 
         public Emitter()
@@ -22,11 +24,31 @@ namespace Codehard.CodeSharp
             ID = Guid.NewGuid();
         }
 
+        private void _Create(string Event, ActionType Callback)
+        {
+            var EventList = GetEventList(this.ID, Event);
+
+            if (EventList != null)
+            {
+                EventList.Add(Callback);
+            }
+            else
+            {
+                EventCollection.Add(new CollectionType(this.ID, Event, new List<ActionType> { Callback }));
+            }
+        }
+
+        private void _Destroy(List<ActionType> EventList, Action<object> Callback)
+        {
+            EventList.RemoveAll(x => x.Item1 == Callback);
+        }
+
         private static void Invoke(List<ActionType> EventList, object Value)
         {
             if (EventList != null)
             {
-                Parallel.Invoke(EventList.Select(x => (Action)(() => x(Value))).ToArray());
+                Parallel.Invoke(EventList.Select(x => (Action)(() => x.Item1(Value))).ToArray());
+                EventList.RemoveAll(x => x.Item2);
             }
         }
 
@@ -38,7 +60,7 @@ namespace Codehard.CodeSharp
         private static List<ActionType> GetEventList(Guid ID, string Event)
         {
             return EventCollection.Where(x => x.Item1 == ID && x.Item2 == Event).Select(x => x.Item3).FirstOrDefault();
-        }        
+        }
 
         public void Emit(string Event, object Value)
         {
@@ -50,18 +72,47 @@ namespace Codehard.CodeSharp
             Invoke(GetEventList(Event), Value);
         }
 
-        public void On(string Event, ActionType Callback)
+        public void Once(string Event, Action<object> Callback)
         {
-            var EventList = GetEventList(this.ID, Event);
+            _Create(Event, new ActionType(Callback, true));
+        }
 
-            if (EventList != null)
+        public void On(string Event, Action<object> Callback)
+        {
+            _Create(Event, new ActionType(Callback, false));
+        }
+
+        public void UnBind(string Event, Action<object> Callback)
+        {
+            _Destroy(GetEventList(this.ID, Event), Callback);
+        }
+    }
+
+    public class DataStream : Emitter, IDisposable
+    {
+        private StreamWriter Stream { get; set; }
+        private bool Disposed = false;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposed)
             {
-                EventList.Add(Callback);
+                if (Disposing)
+                {
+                    if (Stream != null)
+                    {
+                        Stream.Dispose();
+                    }
+                }
+
+                Disposed = true;
             }
-            else
-            {
-                EventCollection.Add(new Tuple<Guid, string, List<ActionType>>(this.ID, Event, new List<ActionType> { Callback }));
-            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 
@@ -70,12 +121,15 @@ namespace Codehard.CodeSharp
         static void Main(string[] args)
         {
             Program Event1 = new Program();
-            Event1.On("Hello", (a) => { Console.WriteLine("1" + a); });
-            Event1.Emit("Hello", "Emit");
+            Action<object> b = (a) => { Console.WriteLine("1" + a); };
+            Event1.On("Hello", b);
+            Event1.Emit("Hello", "Emit1");
+            Event1.UnBind("Hello", b);
+            Event1.Emit("Hello", "Emit2");
 
             Program Event2 = new Program();
             Event2.On("Hello", (a) => { Console.WriteLine("2" + a); });
-            Event2.Emit("Hello", "Emit");
+            Event2.Emit("Hello", "Emit3");
 
             Event1.Boardcast("Hello", "Boardcast");
 
